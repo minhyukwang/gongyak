f79a42b9c2a6b86ea77ad65fdbad4069d7cd116c
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2022-04-06 14:44:43 +0900

    사진상세페이지 수정
    스토리보드 대로 가운데로 했으나 위치는 상의후 결정

--- pages/timelines/picture.tsx
+++ pages/timelines/picture.tsx
@@ -4,9 +4,9 @@ import Layout from "../components/layout";
 const Picture: NextPage = () => {
   return (
     <Layout canGoBack>
-
+        <div className=" -mt-12 flex justify-center items-center h-screen">
             <div className="h-72 w-full bg-slate-400"></div>
-
+        </div>
     </Layout>
   );
 };
