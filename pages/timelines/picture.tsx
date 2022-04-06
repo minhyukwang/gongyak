d32cfb7fc01e9e07cad4ec3c4a5269d545e63377
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2022-04-06 14:27:10 +0900

    Create picture.tsx
    민혁이 작업한 페이지의 사진 상세페이지. 아직 연결전

---
+++ pages/timelines/picture.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Layout from "../components/layout";
+
+const Picture: NextPage = () => {
+  return (
+    <Layout canGoBack>
+
+            <div className="h-72 w-full bg-slate-400"></div>
+
+    </Layout>
+  );
+};
+
+export default Picture;
