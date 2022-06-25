7d172cc36628c62ec4b15a57204e16d2c10fabbc
Minhyuk <minhyukit@gmail.com>
2022-06-25 21:05:49 +0900

    Update layout.tsx

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { cls } from "../libs/client/utils";
+import { cls } from "../../libs/client/utils";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
